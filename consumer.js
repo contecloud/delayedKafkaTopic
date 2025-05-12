75638f331e8b6728885736ff0340f02dfd4d2578
F1728819 Carlo Conte <carlo.conte@bb.com.br>
2025-05-12 16:30:41 -0300

    Evolui a solução

--- consumer.js
+++ consumer.js
@@ -10,29 +10,34 @@ const topic = 'topic-test'
 const consumer = kafka.consumer({ groupId: 'test-group' })
 const producer = kafka.producer({ createPartitioner: Partitioners.DefaultPartitioner })
 
-const sendMessage = (message) => {
+const sendMessage = (message, partition, consumer) => {
     console.log("Enviando novamente para a fila");
-    
+
     return producer.send({
         topic,
         messages: [
             { value: message }
         ]
     })
+
+    consumer.commitOffsets([
+        { topic: 'topic-test', partition, offset: message.offset }
+    ])
 }
 
 const run = async () => {
     await consumer.connect()
     await consumer.subscribe({ topic, fromBeginning: true })
     await consumer.run({
+        autoCommit: false,
         eachMessage: async ({ topic, partition, message }) => {
             const jsonMessage = JSON.parse(message.value.toString())
-            console.log(jsonMessage)
-            if (jsonMessage.valor <= 4) {
+            console.log(new Date(), jsonMessage)
+            if (jsonMessage.valor <= 3) {
                 jsonMessage.valor++
                 const message = JSON.stringify(jsonMessage)
                 await producer.connect()
-                setTimeout(sendMessage, 5000, message)
+                setTimeout(sendMessage(message, partition, consumer), 5000)
             }
         },
     })
--- producer.js
+++ producer.js
@@ -1,26 +1,29 @@
 const { Kafka, logLevel, Partitioners } = require('kafkajs')
 
 const kafka = new Kafka({
-    logLevel: logLevel.INFO,
-    brokers: ['localhost:9092'],
-    clientId: 'example-consumer',
-  })
+  logLevel: logLevel.INFO,
+  brokers: ['localhost:9092'],
+  clientId: 'example-consumer',
+})
 
 const producer = kafka.producer({ createPartitioner: Partitioners.DefaultPartitioner })
 
-const jsonMessage = JSON.stringify({ valor: 1 })
-
-const run = async () => {
-    await producer.connect()
-    await producer.send({
-      topic: 'topic-test',
-      messages: [
-        { 
-            value: jsonMessage
-        },
-      ],
-    })
-    await producer.disconnect()
+const run = async (mensagem) => {
+  await producer.connect()
+  await producer.send({
+    topic: 'topic-test',
+    messages: [
+      {
+        value: mensagem
+      },
+    ],
+  })
+  await producer.disconnect()
 }
 
-run().catch(e => console.error(`[example/consumer] ${e.message}`, e))
+for (let i = 1; i < 30; i++) {
+  setTimeout(() => {
+    const mensagem = JSON.stringify({ mensagem: i, valor: 1 });
+    run(mensagem).catch(e => console.error(`[example/consumer] ${e.message}`, e))
+  }, i * 1000)
+}
